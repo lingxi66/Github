0f7a53a6829e1f6dddfb6c893d30b65959e7f477
007xianyu <113767461+007xianyu@users.noreply.github.com>
2022-11-03 17:06:45 +0800

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 public class App {
     public App(){
-        System.out.println("wuyu");
+        System.out.println("this is an important course");
         System.out.println("芜湖，起飞");
     }
 }
